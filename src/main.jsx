bb0e28065626165e5b891b543ab36ce14d3d183a
nt-leoleo <pederneraleonardo729@gmail.com>
2026-01-26 11:50:02 -0300

    prueba2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import Header from './Header.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <h1>hola</h1>
     {/* <main>
       <ShowpH />
     </main> */}
