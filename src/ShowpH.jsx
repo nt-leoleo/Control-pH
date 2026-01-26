1a9bb1950be7b717312f796acebe3e449da8a656
nt-leoleo <pederneraleonardo729@gmail.com>
2026-01-26 12:11:52 -0300

    mostrar pH

---
+++ src/ShowpH.jsx
@@ -0,0 +1,13 @@
+import './ShowpH.css'
+const ShowpH = () => {
+    return (
+        <div className='body'>
+            <span className='ph'>pH</span>
+            <b>7.1</b>
+            <span className='state'>Estado: OK</span>
+        
+        </div>
+    );
+};
+
+export default ShowpH;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Header from './Header.jsx';
-// import ShowpH from './ShowpH.jsx'
+import ShowpH from './ShowpH.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    {/* <main>
+    <main>
       <ShowpH />
-    </main> */}
+    </main>
   </StrictMode>,
 )
