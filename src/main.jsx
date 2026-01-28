c946ac05e3ca4039402952a997ad3b93d662492a
nt-leoleo <pederneraleonardo729@gmail.com>
2026-01-28 09:49:31 -0300

    fix main import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./App.jsx";
 import { PHProvider } from "./PHContext";
 import "./index.css";
 
