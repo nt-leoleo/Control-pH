b6e7f0e09528bbcf0a0f3e20040c0087e0d2046d
nt-leoleo <pederneraleonardo729@gmail.com>
2026-02-25 14:28:08 -0300

    v4.11.1 revertir login nativo de Google a popup tradicional

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { PHProvider } from "./PHContext";
 import ErrorBoundary from "./ErrorBoundary";
-import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 import "./index.css";
-
-// Inicializar Google Auth para web
-GoogleAuth.initialize({
-  clientId: '102545501878-28h1bok5jkkpbo4309f8spn7svukoo28.apps.googleusercontent.com',
-  scopes: ['profile', 'email'],
-  grantOfflineAccess: true,
-});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
