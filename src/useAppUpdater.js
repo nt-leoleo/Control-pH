76dd9ea5313c384e89ac1ac787033b3c0529e603
nt-leoleo <pederneraleonardo729@gmail.com>
2026-02-25 16:01:53 -0300

    v4.11.5-safe-area-header

--- src/useAppUpdater.js
+++ src/useAppUpdater.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from './firebase';
 
-const CURRENT_VERSION = '4.11.4';
+const CURRENT_VERSION = '4.11.5';
 const UPDATE_CHECK_KEY = 'lastUpdateCheck';
 const CHECK_INTERVAL = 1000 * 60 * 60; // 1 hora
 
