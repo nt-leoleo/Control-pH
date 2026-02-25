f5fee665244b033d5b47a08f866f0e2d59a4338e
nt-leoleo <pederneraleonardo729@gmail.com>
2026-02-25 13:42:37 -0300

    v4.10.5 Botón descarga muestra APP en móvil

--- src/Header.jsx
+++ src/Header.jsx
@@ -93,7 +93,8 @@ const Header = ({ onConfigClick }) => {
                 <polyline points="7 10 12 15 17 10" />
                 <line x1="12" y1="15" x2="12" y2="3" />
               </svg>
-              <span>Descargar App</span>
+              <span className="download-app-text-desktop">Descargar App</span>
+              <span className="download-app-text-mobile">APP</span>
             </button>
           )}
           {user && (
