e7088a68c626ed0fb55b9a9ae86fac87a5d32d5b
nt-leoleo <pederneraleonardo729@gmail.com>
2026-02-26 08:19:05 -0300

    v4.11.11-statusbar-plugin

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { PHProvider } from "./PHContext";
 import ErrorBoundary from "./ErrorBoundary";
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import "./index.css";
+
+// Configurar StatusBar para Android
+if (Capacitor.isNativePlatform()) {
+  StatusBar.setOverlaysWebView({ overlay: true }).catch(err => {
+    console.log('StatusBar overlay error:', err);
+  });
+  
+  StatusBar.setStyle({ style: Style.Dark }).catch(err => {
+    console.log('StatusBar style error:', err);
+  });
+  
+  StatusBar.setBackgroundColor({ color: '#09141d' }).catch(err => {
+    console.log('StatusBar background error:', err);
+  });
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
